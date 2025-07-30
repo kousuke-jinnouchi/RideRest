35f86969b5443c3ebb219e2d15a0f1438a858fce
kousuke-jinnouchi <215395544+kousuke-jinnouchi@users.noreply.github.com>
2025-07-30 17:14:59 +0000

    [Add] Bootstrapの導入

--- app/controllers/parking_lots_controller.rb
+++ app/controllers/parking_lots_controller.rb
@@ -1,10 +1,26 @@
 class ParkingLotsController < ApplicationController
   def new
+    @parking_lot = ParkingLot.new
+  end
+
+  def create
+    @parking_lot = ParkingLot.new(parking_lot_params)
+    @parking_lot.user_id = current_user.id 
+    @parking_lot.save
+    redirect_to parking_lots_path
   end
 
   def index
+    @parking_lots = ParkingLot.all
   end
 
   def show
+    @parking_lot = ParkingLot.find(params[:id])
+  end
+
+  private
+
+  def parking_lot_params
+    params.require(:parking_lot).permit(:genre_id, :parking_lot_name, :address, :latitude, :longitude, :fee, :description, :image)
   end
 end
