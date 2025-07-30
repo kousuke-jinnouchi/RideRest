97f0f7eef4382dc2776a209e1c1479725f87d5fa
kousuke-jinnouchi <215395544+kousuke-jinnouchi@users.noreply.github.com>
2025-07-30 17:14:59 +0000

    [Add] Bootstrapの導入

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
 
-  resources :parking_lots, only: [:new, :index, :show]
+  resources :parking_lots, only: [:new, :create, :index, :show]
   
   get 'homes/about' => 'homes#about', as: 'about' 
 end
