c3c715ed48e755201a798f5e1ed4fc382cc4fae5
kousuke-jinnouchi <215395544+kousuke-jinnouchi@users.noreply.github.com>
2025-08-07 12:35:38 +0000

    実装フェーズ1完了

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :user_state, only: [:create]
 
   def after_sign_in_path_for(resource)
     parking_lots_path
@@ -11,34 +12,22 @@ class Public::SessionsController < Devise::SessionsController
     root_path
   end
 
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
-
   protected
  
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) 
+  end
+
+  private
+
+  def user_state
+    user = User.find_by(email: params[:user][:email])
+    return if user.nil?
+    return unless user.valid_password?(params[:user][:password])
+    unless user.is_active
+      flash[:alert] = "あなたはすでに退会済みです。 再度ご登録をしてご利用ください。"
+      redirect_to new_user_session_path
+    end
   end
   
 end
