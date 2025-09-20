255c49c7d12127fd523de1a85a12a1f1c549bfc9
kousuke-jinnouchi <215395544+kousuke-jinnouchi@users.noreply.github.com>
2025-09-20 10:14:58 +0000

    [Add] ゲストログイン機能の追加

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,5 +1,6 @@
 class Public::UsersController < ApplicationController
   before_action :is_maching_login_user, only: [:edit, :update]
+  before_action :ensure_guest_user, only: [:edit]
   
   def show
     @user = User.find(params[:id])
@@ -44,6 +45,13 @@ class Public::UsersController < ApplicationController
     user = User.find(params[:id])
     unless user.id == current_user.id
       redirect_to parking_lots_path
+    end
+  end
+
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.email == "guest@example.com"
+      redirect_to user_path(current_user), notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
     end
   end
 
